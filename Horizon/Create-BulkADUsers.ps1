fabc19070b8bce277e0113a5b147ce4c58950652
Daniël Paessens <dpaessens@hotmail.com>
2021-10-26 14:04:11 +0200

    Create multiple User accounts

---
+++ Horizon/Create-BulkADUsers.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module ActiveDirectory
+$totalusers = 10
+for ($i=0; $i -lt $totalusers; $i++) 
+ { 
+ $userID = "{0:00}" -f ($i + 1)
+ $userName = "TestUser$userID"
+ 
+ Write-Host "Creating AD user" ($i + 1) "of" $totalusers ":" $userName
+ 
+New-ADUser `
+ -Name $userName  `
+ -Path  "CN=Users,DC=elegis,DC=be" `
+ -SamAccountName $userName `
+ -AccountPassword (ConvertTo-SecureString "Azerty+123" -AsPlainText -Force) `
+ -Enabled $true
+ Add-ADGroupMember "Horizon Test Users" $userName;
+}
